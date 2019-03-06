57e6fa2520e59807bbdb302cb50294b442756d7e
klrc <1440698245@qq.com>
2019-03-06 16:40:16 +0800

    reformated

--- preprocessing.py
+++ preprocessing.py
@@ -1,68 +1,82 @@
+import glob
+import os.path
+import numpy as np
 import os
 import shutil
-
 from PIL import Image
-
-ext = ['jpg', 'jpeg', 'png']
-files = os.listdir('.')
-
-
-def process_image_deprecated(image, mwidth=200, mheight=150):
-    w, h = image.size
-    if w <= mwidth and h <= mheight:
-        return
-    if (1.0 * w / mwidth) > (1.0 * h / mheight):
-        scale = 1.0 * w / mwidth
-        new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-
-    else:
-        scale = 1.0 * h / mheight
-        new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-    return new_im
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-def process_image(image, width=200, height=150):
-    new_img = image.resize((width, height), Image.BILINEAR)
-    return new_img
+def resize(input_img, output_img, width=200, height=150):
+    img = Image.open(input_img)
+    try:
+        img = img.resize((width, height), Image.BILINEAR)
+        img.save(output_img)
+        img.close()
+    except Exception as e:
+        print(e)
 
 
-def mkdirs():
-    os.mkdir('resized')
-    os.mkdir('resized/img')
-    os.mkdir('resized/mask')
-    os.mkdir('resized/yaml')
+def _mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
-def process(raw_path='raw_imgs', maxidx=-1, target=None):
-    foods = os.listdir(raw_path)
-    for food in foods:
-        if target and food not in target:
-            continue
-        fp = f'{raw_path}/{food}'
-        img_folders = os.listdir(fp)
-        img_folders = [x for x in img_folders if x.endswith('_json')]
-        for folder in img_folders:
-            img_id = folder.split('_')[-2]
-            with open(f'{fp}/{folder}/label_names.txt') as f:
-                f.readline()
-                img_class = f.readline().replace('\n', '')
-            img_id = f'{food}{img_id}'
-            print(img_id)
-            image = Image.open(f'{fp}/{folder}/img.png')
-            image = process_image(image)
-            image.save(f'resized/img/{img_id}.png')
-            image.close()
-            mask = Image.open(f'{fp}/{folder}/label.png')
-            mask = process_image(mask)
-            mask.save(f'resized/mask/{img_id}.{img_class}.png')
-            mask.close()
-            shutil.copyfile(f'{fp}/{folder}/info.yaml', f'resized/yaml/{img_id}.yaml')
-        maxidx -= 1
-        if maxidx == 0:
-            break
+def mkdirs(path):
+    _mkdir(path)
+    for x in ['train', 'test']:
+        _mkdir(f'{path}/{x}')
+        _mkdir(f'{path}/{x}/img')
+        _mkdir(f'{path}/{x}/mask')
+        _mkdir(f'{path}/{x}/yaml')
+
+
+def scan_data(*dirs):
+    for path in dirs:
+        foods = os.listdir(path)
+        for food in foods:  # 进入菜品文件夹
+            if '.' in food:
+                continue
+            fp = f'{path}/{food}'  # 食物/包子/
+            img_folders = os.listdir(fp)  # 包子内所有文件夹和文件  -  列表
+            img_folders = [x for x in img_folders if x.endswith('_json')]
+            for x in img_folders:
+                yield f'{fp}/{x}'
+
+
+def divide_feeder(data_list):
+    np.random.shuffle(data_list)
+    _divide_idx = int(len(data_list)/5)
+    return data_list[0:_divide_idx], data_list[_divide_idx:-1]
+
+
+def gen_feeder(data, output_dir):
+    global TOTAL_TASKS, DONE_TASKS
+    img_id = data.split('/')[-1].split('_json')[0]
+    print(f'processing {img_id} .. ({DONE_TASKS}/{TOTAL_TASKS})')
+    DONE_TASKS += 1
+
+    resize(f'{data}/img.png', f'{output_dir}/img/{img_id}.png')
+    resize(f'{data}/label.png', f'{output_dir}/mask/{img_id}.png')
+    shutil.copyfile(f'{data}/info.yaml', f'{output_dir}/yaml/{img_id}.yaml')
 
 
 if __name__ == '__main__':
-    mkdirs()
-    target = ('包子', '猪肉白菜水饺', '清炒土豆丝')
-    process(raw_path='network_imgs', target=target)
+
+    feeder_dir = '.feeder'
+    mkdirs(feeder_dir)
+
+    data_list = [x for x in scan_data(
+        '/run/media/sh/My Passport/erkeyiyuan/数据/网络/netfood',
+        '/run/media/sh/My Passport/erkeyiyuan/数据/本地菜品/压缩包')]
+
+    TOTAL_TASKS = len(data_list)
+    DONE_TASKS = 0
+
+    test, train = divide_feeder(data_list)
+
+    for x in test:
+        gen_feeder(x, f'{feeder_dir}/test')
+    for x in train:
+        gen_feeder(x, f'{feeder_dir}/train')
